946987f085a80bfc2675e393ba9f5db5dcef1dca
jantiks <58886410+jantiks@users.noreply.github.com>
2020-11-19 17:04:28 +0400

    mainstoryboard is ready and default page also is ready

--- browser/ViewController.swift
+++ browser/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var urlTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let homePage = "https://www.apple.com"
+        let url = URL(string: homePage)
+        let request = URLRequest(url: url!)
+        
+        webView.load(request)
+        webView.allowsBackForwardNavigationGestures = true
+        
+        urlTextField.text = homePage
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func backTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func forwardTapped(_ sender: UIButton) {
+    }
+    
+    
+}
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let urlString = textField.text!
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        
+        webView.load(request)
+        
+        return true
+        
+        
+    }
 }
 
